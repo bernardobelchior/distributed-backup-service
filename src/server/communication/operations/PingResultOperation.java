d8018500531db631a8a0290845909459e36167ba
Bernardo Belchior <bernardo.belchior1@gmail.com>
2017-05-25 17:45:58 +0100

    Node bootstrap failing.

--- src/server/communication/operations/PingResultOperation.java
+++ src/server/communication/operations/PingResultOperation.java
@@ -4,18 +4,14 @@ import server.chord.Node;
 import server.chord.NodeInfo;
 import server.communication.Operation;
 
-/**
- * Created by epassos on 5/25/17.
- */
 public class PingResponseOperation extends Operation {
 
-    public PingResponseOperation(NodeInfo origin){
+    public PingResponseOperation(NodeInfo origin) {
         super(origin);
     }
 
     @Override
     public void run(Node currentNode) {
-        System.out.println("Received ping response from node with ID " + origin.getId());
         currentNode.onPingResponse(origin);
     }
 }
